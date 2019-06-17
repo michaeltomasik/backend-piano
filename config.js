cadf3384df38029e724e8b1fae84c4f23e258b9e
michal <shugo12345@gmail.com>
2019-06-17 02:11:57 +0200

    Modify url for remote server

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-
-const url = 'mongodb://localhost:27017/graphqldb';
+const url = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0-l9bnm.mongodb.net/test?retryWrites=true&w=majority`;
+// const url = 'mongodb://localhost:27017/graphqldb';
 
 mongoose.connect(url, { useNewUrlParser: true });
 mongoose.connection.once('open', () => console.log(`Connected to mongo at ${url}`));
